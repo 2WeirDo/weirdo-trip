863ad33c59d96bd0de92df39583dab5783451d0e
zweirdo <1606403438@qq.com>
2023-07-17 16:57:55 +0800

    增添了收藏, 订单, 消息页面

--- src/views/message/message.vue
+++ src/views/message/message.vue
@@ -1,15 +1,27 @@
 <template>
-    <div class="massage top-page">
-        <h2>massage</h2>
+    <div class="message">
+      <nav-bar title="消息中心"></nav-bar>
+      <div class="content">
+        <img src="@/assets/img/message/icon_message.png" alt="">
+        <div class="text">暂无聊天消息</div>
+      </div>
     </div>
-</template>
-
-
-<script setup>
-
-</script>
-
-
-<style lang="less" scoped>
-
-</style>
+  </template>
+  
+  <script setup>
+  import NavBar from "@/components/nav-bar/nav-bar.vue"
+  
+  </script>
+  
+  <style lang="less" scoped>
+    .content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 100px;
+      img {
+        width: 88%;
+      }
+    }
+  </style>
+  
